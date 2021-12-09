2cc1418913e446d1b0c5dacf499372e9e182c433
gredey <osaidoupro@gmail.com>
2021-12-09 19:32:28 +0000

    methode norme

--- vector.py
+++ vector.py
@@ -1,4 +1,10 @@
+import math
+
 class vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def norme(self):
+        n = math.sqrt(self.x**2 + self.y**2)
+        return n
